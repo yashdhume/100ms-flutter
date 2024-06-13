a24582138106d60880b4c2085186f9928d08ded1
Yash Dhume <yash.dhume@gmail.com>
2024-06-13 12:27:08 -0700

    feat: change force change role logic

--- packages/hms_room_kit/lib/src/meeting/meeting_store.dart
+++ packages/hms_room_kit/lib/src/meeting/meeting_store.dart
@@ -5,25 +5,24 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
-//Package imports
-// import 'package:hms_video_plugin/hms_video_plugin.dart';
-import 'package:hmssdk_flutter/hmssdk_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
-
-//Project imports
-import 'package:hms_room_kit/src/model/poll_store.dart';
-import 'package:hms_room_kit/src/model/participant_store.dart';
 import 'package:hms_room_kit/hms_room_kit.dart';
 import 'package:hms_room_kit/src/enums/meeting_mode.dart';
 import 'package:hms_room_kit/src/enums/session_store_keys.dart';
 import 'package:hms_room_kit/src/hmssdk_interactor.dart';
 import 'package:hms_room_kit/src/layout_api/hms_room_layout.dart';
+import 'package:hms_room_kit/src/model/participant_store.dart';
 import 'package:hms_room_kit/src/model/peer_track_node.dart';
+//Project imports
+import 'package:hms_room_kit/src/model/poll_store.dart';
 import 'package:hms_room_kit/src/model/rtc_stats.dart';
 import 'package:hms_room_kit/src/widgets/toasts/hms_toast_model.dart';
 import 'package:hms_room_kit/src/widgets/toasts/hms_toasts_type.dart';
+//Package imports
+// import 'package:hms_video_plugin/hms_video_plugin.dart';
+import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:intl/intl.dart';
 
 ///[MeetingStore] is the store that is used to store the data of the meeting
 ///It takes the following parameters:
@@ -518,14 +517,12 @@ class MeetingStore extends ChangeNotifier
   }
 
   bool shouldForceChange(String currentRole, String newRole) {
-    if (currentRole == 'video' &&
-        (newRole == 'voice' || newRole == 'chat' || newRole == 'spectator')) {
-      return true;
-    } else if (currentRole == 'voice' &&
-        (newRole == 'chat' || newRole == 'spectator')) {
-      return true;
-    }
-    return false;
+    Map<String, List<String>> validRoles = {
+      'video': ['voice', 'chat', 'spectator'],
+      'voice': ['chat', 'spectator']
+    };
+
+    return validRoles[currentRole]?.contains(newRole) ?? false;
   }
 
   void changeRole(HMSPeer peer, String roleName) {
@@ -739,6 +736,7 @@ class MeetingStore extends ChangeNotifier
   }
 
   HMSHLSRecordingConfig? hmshlsRecordingConfig;
+
   Future<HMSException?> startHLSStreaming(
       bool singleFile, bool videoOnDemand) async {
     hmshlsRecordingConfig = HMSHLSRecordingConfig(
@@ -2521,7 +2519,7 @@ class MeetingStore extends ChangeNotifier
       /*******************************This is the implementation for showing emoji's in HLS *******************/
       /**
        * Generally we are assuming that the timed metadata payload will be a JSON String
-       * but if it's a normal string then this throws the format exception 
+       * but if it's a normal string then this throws the format exception
        * Hence we catch it and display the payload as string on toast.
        * The toast is displayed for the time duration hlsCue.endDate - hlsCue.startDate
        * If endDate is null then toast is displayed for 2 seconds by default
