f706ff71a1c0e8b57f655fa237d53ee35a031ecb
Yash Dhume <yash.dhume@gmail.com>
2024-07-03 21:05:31 -0700

    fix: solve only participant in call issue

--- packages/hms_room_kit/lib/src/meeting/meeting_grid_component.dart
+++ packages/hms_room_kit/lib/src/meeting/meeting_grid_component.dart
@@ -1,23 +1,20 @@
 ///Dart imports
 library;
 
-import 'dart:io';
-
 ///Package imports
 import 'package:flutter/material.dart';
-import 'package:hms_room_kit/src/meeting/waiting_room_screen.dart';
-import 'package:hmssdk_flutter/hmssdk_flutter.dart';
-import 'package:lottie/lottie.dart';
-import 'package:provider/provider.dart';
-import 'package:tuple/tuple.dart';
 
 ///Project imports
 import 'package:hms_room_kit/src/enums/meeting_mode.dart';
 import 'package:hms_room_kit/src/meeting/meeting_navigation_visibility_controller.dart';
 import 'package:hms_room_kit/src/meeting/meeting_store.dart';
+import 'package:hms_room_kit/src/meeting/waiting_room_screen.dart';
 import 'package:hms_room_kit/src/model/peer_track_node.dart';
 import 'package:hms_room_kit/src/widgets/meeting_modes/custom_one_to_one_grid.dart';
 import 'package:hms_room_kit/src/widgets/meeting_modes/one_to_one_mode.dart';
+import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
 
 ///[MeetingGridComponent] is a component that is used to show the video grid
 class MeetingGridComponent extends StatelessWidget {
@@ -82,14 +79,19 @@ class MeetingGridComponent extends StatelessWidget {
                                                 .localPeer
                                                 ?.videoTrack !=
                                             null))
-                                ? OneToOneMode(
-                                    ///This is done to keep the inset tile
-                                    ///at correct position when controls are hidden
-                                    bottomMargin: showControls ? 250 : 130,
-                                    peerTracks: data.item1,
-                                    screenShareCount: data.item4,
-                                    context: context,
-                                  )
+                                ? data.item3 == 1
+                                    ? CustomOneToOneGrid(
+                                        isLocalInsetPresent: false,
+                                        peerTracks: data.item1,
+                                      )
+                                    : OneToOneMode(
+                                        ///This is done to keep the inset tile
+                                        ///at correct position when controls are hidden
+                                        bottomMargin: showControls ? 250 : 130,
+                                        peerTracks: data.item1,
+                                        screenShareCount: data.item4,
+                                        context: context,
+                                      )
                                 : CustomOneToOneGrid(
                                     isLocalInsetPresent: false,
                                     peerTracks: data.item1,
