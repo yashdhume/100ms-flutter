962506179a0b06f6c490aa14ec4b22b98de59a8c
Yash Dhume <yash.dhume@gmail.com>
2024-08-02 18:03:06 -0700

    feat: changed scaled back

--- packages/hms_room_kit/lib/src/widgets/grid_layouts/listenable_peer_widget.dart
+++ packages/hms_room_kit/lib/src/widgets/grid_layouts/listenable_peer_widget.dart
@@ -20,7 +20,7 @@ class ListenablePeerWidget extends StatelessWidget {
       {super.key,
       required this.index,
       required this.peerTracks,
-      this.scaleType = ScaleType.SCALE_ASPECT_FIT});
+      this.scaleType = ScaleType.SCALE_ASPECT_FILL});
 
   @override
   Widget build(BuildContext context) {
