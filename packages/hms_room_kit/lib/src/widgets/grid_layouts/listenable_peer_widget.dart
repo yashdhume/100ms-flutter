c1a65eeae74464cc5c5148692ca62b24cf5a3591
Yash Dhume <yash.dhume@gmail.com>
2024-08-02 17:29:57 -0700

    feat: make scale type to fit

--- packages/hms_room_kit/lib/src/widgets/grid_layouts/listenable_peer_widget.dart
+++ packages/hms_room_kit/lib/src/widgets/grid_layouts/listenable_peer_widget.dart
@@ -3,12 +3,12 @@ library;
 
 import 'package:flutter/cupertino.dart';
 import 'package:hms_room_kit/src/meeting/meeting_store.dart';
-import 'package:provider/provider.dart';
 
 ///Project imports
 import 'package:hms_room_kit/src/model/peer_track_node.dart';
 import 'package:hms_room_kit/src/widgets/common_widgets/peer_tile.dart';
 import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:provider/provider.dart';
 
 ///This widget is used to render the peer tile
 class ListenablePeerWidget extends StatelessWidget {
@@ -20,7 +20,7 @@ class ListenablePeerWidget extends StatelessWidget {
       {super.key,
       required this.index,
       required this.peerTracks,
-      this.scaleType = ScaleType.SCALE_ASPECT_FILL});
+      this.scaleType = ScaleType.SCALE_ASPECT_FIT});
 
   @override
   Widget build(BuildContext context) {
